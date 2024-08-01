50cc1e6d1929b48d48cedd63636e9b48858f5de6
kaziabulhasib <hasibkazi420@gmail.com>
2024-08-01 22:18:09 +0530

    add:services by id route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -33,6 +33,13 @@ async function run() {
     const serviceCollection = client.db("carDoctor2").collection("services");
     app.get("/services", async (req, res) => {
       const result = await serviceCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
       res.send(result);
     });
 
