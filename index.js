08951c08176b21872c3a1cb87004263061c7018c
kaziabulhasib <hasibkazi420@gmail.com>
2024-08-01 19:35:33 +0530

    server setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle ware
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Car Doctor is runnng ....");
+});
+
+app.listen(port, () => console.log("server is running on ", port));
